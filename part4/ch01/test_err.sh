e2aa9dc2af0addfe4d1025bc5d219c20872285a1
gurumee92 <gurumee92@gmail.com>
2021-06-26 16:26:42 +0900

    Update 04/01

---
+++ part4/ch01/test_err.sh
@@ -0,0 +1,22 @@
+while :
+do
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/err) &
+  (curl localhost:8080/) &
+  (curl localhost:8080/) &
+  (curl localhost:8080/) &
+  (curl localhost:8080/) &
+  (curl localhost:8080/) &
+  (curl localhost:8080/) &
+  sleep 1
+done
